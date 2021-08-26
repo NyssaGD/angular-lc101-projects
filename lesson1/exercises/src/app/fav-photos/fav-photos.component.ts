50ccf5943c5c90276242c836f6445a6e39f3a7e2
Nyssa <nyssagd@gmail.com>
2021-08-26 18:14:34 -0400

    classtime!

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
-  image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  photosTitle = 'Raccoon Photos';
+  image1 = 'https://www.skedaddlewildlife.com/wp-content/uploads/2019/06/raccoon-1024x853.jpg';
+  image2 = 'https://64.media.tumblr.com/7119eb2a6b8cf061127002fb67a4ef9f/tumblr_oisf7koWBj1ueus7io1_500.jpg';
+  image3 = 'https://www.ladbible.com/cdn-cgi/image/width=1200,quality=70,format=jpeg,fit=contain,dpr=1/https%3A%2F%2Fs3-images.ladbible.com%2Fs3%2Fcontent%2Fca17d8185f2b86f41e798a36bfe657c6.png';
 
   constructor() { }
 
